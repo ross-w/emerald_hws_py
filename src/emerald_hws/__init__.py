ab93bfe73d5cd286edf02c4d650398ec69cc4c82
ross-w <ross@inertia.net.nz>
2026-09-17 21:20:26 +1000

    feat: exception handling for API errors (#61)

--- src/emerald_hws/__init__.py
+++ src/emerald_hws/__init__.py
@@ -1,9 +1,17 @@
 from .emeraldhws import EmeraldHWS
-from .exceptions import EmeraldConnectionError, EmeraldError, EmeraldTimeoutError
+from .exceptions import (
+    EmeraldApiError,
+    EmeraldAuthError,
+    EmeraldConnectionError,
+    EmeraldError,
+    EmeraldTimeoutError,
+)
 
 __all__ = [
     "EmeraldHWS",
     "EmeraldError",
+    "EmeraldApiError",
+    "EmeraldAuthError",
     "EmeraldConnectionError",
     "EmeraldTimeoutError",
 ]
